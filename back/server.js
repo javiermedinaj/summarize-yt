5970226f4a88abcc1a9d2cc2e7cb26845405f744
medinajavierj <javier_j_medina@hotmail.com>
2024-11-12 21:45:44 -0300

    wf

--- back/server.js
+++ back/server.js
@@ -5,22 +5,9 @@ import flashcardRouter from './routes/flashcards.routes.js';
 
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:5173', 
-    'https://summarize-yt.vercel.app'
-];
-
 app.use(cors({
-    origin: function(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'OPTIONS'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
 app.use(express.json());
