6f26105f88bd68a2bf2678ba50b63cef7ca22309
Javi Medina <javier_j_medina@hotmail.com>
2025-08-31 18:30:57 -0300

    vercel

--- back/index.js
+++ back/index.js
@@ -5,6 +5,12 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Para desarrollo local
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+// Para Vercel
+export default app;
